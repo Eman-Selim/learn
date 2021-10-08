b0a2c5411b42dc789490ae7870c4d6051ba19a27
Eman-Selim <emanselim305@gmail.com>
2021-10-08 11:45:54 +0200

    blade

--- routes/web.php
+++ routes/web.php
@@ -17,28 +17,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//route for users - returning a string
-Route::get('/users', function () {
-    return 'welcome to the users route';
-});
-Route::get('eman', function(){
-    return 'hey, are you good';
-});
-//route to users returning array 
-Route::get('/users',function(){
-    return ['Eman','Asmaa','Alaa','Ibrahim','Ibtehal'];
-});
-//Route to users returning JSON object 
-Route::get('users',function(){
-    return response()->json([
-        'name'=>'eman',
-        'couse'=>'laravel beginner to advanced'
-    ]);
-});
-//route to users returning a function
-Route::get('/users',function(){
-    return redirect('/');
-});
+// //route for users - returning a string
+// Route::get('/users', function () {
+//     return 'welcome to the users route';
+// });
+// Route::get('eman', function(){
+//     return 'hey, are you good';
+// });
+// //route to users returning array 
+// Route::get('/users',function(){
+//     return ['Eman','Asmaa','Alaa','Ibrahim','Ibtehal'];
+// });
+// //Route to users returning JSON object 
+// Route::get('users',function(){
+//     return response()->json([
+//         'name'=>'eman',
+//         'couse'=>'laravel beginner to advanced'
+//     ]);
+// });
+// //route to users returning a function
+// Route::get('/users',function(){
+//     return redirect('/');
+// });
 
-//firsproject.com == /
-//firstproject.com/users ==/users
+// //firsproject.com == /
+// //firstproject.com/users ==/users
+Route::get('/home',function(){
+    return view('home');
+});
